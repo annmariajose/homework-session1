0978a72b7a063a7eb57d21267b9f8537fbe7c635
Ann Maria Jose <annmariajose1412@gmail.com>
2024-02-07 17:19:20 -0600

    Add cliff location to display box

--- js/activity.js
+++ js/activity.js
@@ -1,12 +1,13 @@
 //user interaction with table cells
 $(document).ready(function() {
     $("table tbody tr td:not(:first-child)").hover(function() { //user hover a table data cell
-        var content = $(this).text(); //get content of cell
-
         if ($(this).text() != "Not Available") { //check if content does not contain a particular string
             $(this).css('cursor', 'pointer'); //change mouse pointer
 
             $(this).click(function() { //user select a table data cell
+                var cliffName = $("table thead tr th:eq(" + $(this).index() + ")").text(); //get cliff location
+                var content = $(this).text() + " at " + cliffName; //get content of cell
+
                 $(this).toggleClass("highlightCell"); //add or remove class when cell is selected
 
                 if ($(this).hasClass("highlightCell")) { //check if selected cell has class
