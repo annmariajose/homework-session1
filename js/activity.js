b0e8e5201b8a667ec09c899713f013de6b7e0cbc
Ann Maria Jose <annmariajose1412@gmail.com>
2024-02-07 17:04:04 -0600

    Add jquery for display box

--- js/activity.js
+++ js/activity.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
-    var notAvaiableText = "Not Available";
     $("table tbody tr td:not(:first-child)").hover(function() {
-        if ($(this).text() != notAvaiableText) {
+        var content = $(this).text();
+
+        if ($(this).text() != "Not Available") {
             $(this).css('cursor', 'pointer');
+
             $(this).click(function() {
                 $(this).toggleClass("highlightCell");
+                
+                if ($(this).hasClass("highlightCell")) {
+                    $("#displaySelected").css("visibility", "visible");
+                    $("#displaySelected").css("margin-top", "2em");
+                    $("#result").append("<p>" + content + "</p>");
+                } else {
+                    $("#result p:contains(" + content + ")").remove();
+                    if ($("#result").has("p").length == false) {
+                        $("#displaySelected").css("visibility", "hidden");
+                        $("#displaySelected").css("margin-top", "0");
+                    }
+                }
             })
         }
     })
