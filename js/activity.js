90163955d1e322f5c2c293c999e20713cc4ec7e3
Ann Maria Jose <annmariajose1412@gmail.com>
2024-02-07 17:09:51 -0600

    Add comments

--- js/activity.js
+++ js/activity.js
@@ -1,22 +1,24 @@
+//user interaction with table cells
 $(document).ready(function() {
-    $("table tbody tr td:not(:first-child)").hover(function() {
-        var content = $(this).text();
+    $("table tbody tr td:not(:first-child)").hover(function() { //user hover a table data cell
+        var content = $(this).text(); //get content of cell
 
-        if ($(this).text() != "Not Available") {
-            $(this).css('cursor', 'pointer');
+        if ($(this).text() != "Not Available") { //check if content does not contain a particular string
+            $(this).css('cursor', 'pointer'); //change mouse pointer
 
-            $(this).click(function() {
-                $(this).toggleClass("highlightCell");
-                
-                if ($(this).hasClass("highlightCell")) {
-                    $("#displaySelected").css("visibility", "visible");
-                    $("#displaySelected").css("margin-top", "2em");
-                    $("#result").append("<p>" + content + "</p>");
-                } else {
-                    $("#result p:contains(" + content + ")").remove();
-                    if ($("#result").has("p").length == false) {
-                        $("#displaySelected").css("visibility", "hidden");
-                        $("#displaySelected").css("margin-top", "0");
+            $(this).click(function() { //user select a table data cell
+                $(this).toggleClass("highlightCell"); //add or remove class when cell is selected
+
+                if ($(this).hasClass("highlightCell")) { //check if selected cell has class
+                    $("#displaySelected").css("visibility", "visible"); //make display box visible
+                    $("#displaySelected").css("margin-top", "2em"); //add spaces above display box
+                    $("#result").append("<p>" + content + "</p>"); //add child element with contents of cell
+                } else { //if selected cell do not have class
+                    $("#result p:contains(" + content + ")").remove(); //remove child element
+
+                    if ($("#result").has("p").length == false) { //check if there are any child within parent
+                        $("#displaySelected").css("visibility", "hidden"); //make display box hidden
+                        $("#displaySelected").css("margin-top", "0"); //remove spaces above display box
                     }
                 }
             })
